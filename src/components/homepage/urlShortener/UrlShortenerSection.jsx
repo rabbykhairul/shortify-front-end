3afc59f18eb59cd19d502076de4e5dc5596db774
rabbykhairul <rabbykhairul@gmail.com>
2020-12-18 19:51:41 +0600

    create UrlShortenerSection with basic styling

---
+++ src/components/homepage/urlShortener/UrlShortenerSection.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+import "./UrlShortenerSection.css";
+
+const UrlShortenerSection = () => {
+  return (
+    <div className="url-shortener-section">
+      <div className="card-style-container"></div>
+    </div>
+  );
+};
+
+export default UrlShortenerSection;
