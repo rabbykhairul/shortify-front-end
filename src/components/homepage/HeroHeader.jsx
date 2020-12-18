34f315f5f29b342b35b85173e6416915127cf7a2
rabbykhairul <rabbykhairul@gmail.com>
2020-12-18 19:31:20 +0600

    create HeroHeader component

---
+++ src/components/homepage/HeroHeader.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+import "./HeroHeader.css";
+
+const HeroHeader = () => {
+  return (
+    <header className="hero-header">
+      <h1>Shortify</h1>
+      <p>Easily shorten big links and enjoy unlimited redirects</p>
+    </header>
+  );
+};
+
+export default HeroHeader;
