52121a13bad94821227623650cac485765c586d2
rabbykhairul <rabbykhairul@gmail.com>
2020-12-18 19:52:29 +0600

    render UrlShortenerSection component inside Homepage

--- src/components/homepage/HomePage.jsx
+++ src/components/homepage/HomePage.jsx
@@ -1,11 +1,13 @@
 import React from "react";
 import HeroHeader from "./HeroHeader";
+import UrlShortenerSection from "./urlShortener/UrlShortenerSection";
 import "./HomePage.css";
 
 const HomePage = () => {
   return (
     <div className="homepage-container">
       <HeroHeader />
+      <UrlShortenerSection />
     </div>
   );
 };
