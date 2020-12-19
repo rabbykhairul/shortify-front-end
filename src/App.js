2c610247bbf0f6679f7aa657d627be79a55eb4d4
rabbykhairul <rabbykhairul@gmail.com>
2020-12-20 00:27:46 +0600

    add route path for home page and redirect page

--- src/App.js
+++ src/App.js
@@ -1,8 +1,15 @@
 import React from "react";
+import { Switch, Route } from "react-router-dom";
 import HomePage from "./components/homepage/HomePage";
+import RedirectPage from "./components/redirectPage/RedirectPage";
 
 function App() {
-  return <HomePage />;
+  return (
+    <Switch>
+      <Route path="/:shortCode" component={RedirectPage} />
+      <Route path="/" exact component={HomePage} />
+    </Switch>
+  );
 }
 
 export default App;
