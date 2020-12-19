de9bbee5ffb00606796c4cc279a37be0ed84d27e
rabbykhairul <rabbykhairul@gmail.com>
2020-12-20 01:51:13 +0600

    try to redirect the user to original url for a given shortcode

--- src/components/redirectPage/RedirectPage.jsx
+++ src/components/redirectPage/RedirectPage.jsx
@@ -1,16 +1,46 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
+import { getOriginalUrl } from "../../services/shortUrlService";
 import "./RedirectPage.css";
 
 const RedirectPage = (props) => {
   const { match } = props;
+  const [errorStatus, setErrorStatus] = useState(false);
+
+  useEffect(() => {
+    const tryToRedirectToOriginalUrl = async () => {
+      const originalURL = await getOriginalUrl(match.params.shortCode);
+      if (originalURL) return (window.location = originalURL);
+      setErrorStatus(true);
+    };
+    tryToRedirectToOriginalUrl();
+  }, [match]);
+
+  const showRedirectMessage = () => {
+    if (!errorStatus)
+      return (
+        <h1>
+          Redirecting
+          <span className="circle-animation">.</span>
+          <span className="circle-animation">.</span>
+          <span className="circle-animation">.</span>
+        </h1>
+      );
+  };
+
+  const showErrorMessage = () => {
+    if (errorStatus)
+      return (
+        <h1 className="error-message">
+          This short url doesn't exist! To create a short url{" "}
+          <span>click here!</span>
+        </h1>
+      );
+  };
+
   return (
     <div className="redirect-page-container">
-      <h1>
-        Redirecting
-        <span className="circle-animation">.</span>
-        <span className="circle-animation">.</span>
-        <span className="circle-animation">.</span>
-      </h1>
+      {showRedirectMessage()}
+      {showErrorMessage()}
     </div>
   );
 };
