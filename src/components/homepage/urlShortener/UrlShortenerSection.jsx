a7424e982509f71b0418e79aa207190ebd1c3ed4
rabbykhairul <rabbykhairul@gmail.com>
2020-12-19 19:26:07 +0600

    render Url shortener form inside Url shortener section

--- src/components/homepage/urlShortener/UrlShortenerSection.jsx
+++ src/components/homepage/urlShortener/UrlShortenerSection.jsx
@@ -1,10 +1,13 @@
 import React from "react";
+import UrlShortenerForm from "./UrlShortenerForm";
 import "./UrlShortenerSection.css";
 
 const UrlShortenerSection = () => {
   return (
     <div className="url-shortener-section">
-      <div className="card-style-container"></div>
+      <div className="card-style-container">
+        <UrlShortenerForm />
+      </div>
     </div>
   );
 };
