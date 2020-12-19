626197660d374fc05b9e3d828681efb772289b84
rabbykhairul <rabbykhairul@gmail.com>
2020-12-19 23:42:43 +0600

    display ajax call animation or short url info when needed

--- src/components/homepage/urlShortener/UrlShortenerSection.jsx
+++ src/components/homepage/urlShortener/UrlShortenerSection.jsx
@@ -1,13 +1,32 @@
 import React, { useState } from "react";
+import { getShortUrl } from "../../../services/shortUrlService";
 import UrlShortenerForm from "./UrlShortenerForm";
 import "./UrlShortenerSection.css";
 
 const UrlShortenerSection = () => {
   const [formSubmitStatus, setFormSubmitStatus] = useState(false);
+  const [
+    shouldDisplayFormSubmitAnimation,
+    setShouldDisplayFormSubmtiAnimation,
+  ] = useState(false);
+  const [shouldDisplayShortUrlsInfo, setShouldDisplayShortUrlInfo] = useState(
+    false
+  );
 
-  const shortenUrl = (originalURL) => {
+  const shortenUrl = async (originalURL) => {
     console.log(originalURL);
     setFormSubmitStatus(true);
+    setShouldDisplayFormSubmtiAnimation(true);
+
+    await getShortUrl(originalURL);
+
+    setShouldDisplayFormSubmtiAnimation(false);
+    setShouldDisplayShortUrlInfo(true);
+  };
+
+  const letUserShortenAnotherLink = () => {
+    setShouldDisplayShortUrlInfo(false);
+    setFormSubmitStatus(false);
   };
 
   const renderUrlShortenerForm = () => {
@@ -15,7 +34,31 @@ const UrlShortenerSection = () => {
   };
 
   const renderFormSubmitAnimation = () => {
-    if (formSubmitStatus) return <div className="dot-animation">...</div>;
+    if (shouldDisplayFormSubmitAnimation)
+      return <div className="dot-animation">...</div>;
+  };
+
+  const renderShortUrlInfo = () => {
+    if (shouldDisplayShortUrlsInfo)
+      return (
+        <>
+          <p className="url-type">Original URL</p>
+          <p className="url-string-box">https://google.com</p>
+          <p className="url-type">Short URL</p>
+          <p className="url-string-box">
+            https://shortify.netlify.app/aBcd8o/abcowoeiwl
+          </p>
+          <div className="copy-btn-container">
+            <button className="copy-btn">Copy short URL!</button>
+          </div>
+          <p
+            className="short-new-url-prompt"
+            onClick={letUserShortenAnotherLink}
+          >
+            Click here to shorten another link.
+          </p>
+        </>
+      );
   };
 
   return (
@@ -23,6 +66,7 @@ const UrlShortenerSection = () => {
       <div className="card-style-container">
         {renderUrlShortenerForm()}
         {renderFormSubmitAnimation()}
+        {renderShortUrlInfo()}
       </div>
     </div>
   );
